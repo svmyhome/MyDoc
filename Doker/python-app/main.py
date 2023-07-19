1f6d1508cff8b999b314e1346c978051277dd598
vladimir <svmyhome@mail.ru>
2023-07-19 21:50:46 +0300

    Added Dockerfiles

---
+++ Doker/python-app/main.py
@@ -0,0 +1,8 @@
+import calendar
+
+print('Welcome 11111111111222222222222222222!!!!')
+year = int(input('Input year '))
+month = int(input('Input month '))
+
+print(calendar.month(year, month))
+print('Bye')
